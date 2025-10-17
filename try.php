020de5193f9db38ca2e87f150e23685a503a143c
Veus-305 <vendrickagie@gmail.com>
2025-10-17 11:10:41 +0100

    first upload from local folder

---
+++ try.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Area/sum of Rectangle </title>
+</head>
+<body>
+    <?php
+    $lenght=17;
+    $width=8;
+    $Area=17*8;
+    $sum=(17*2)+(8*2);
+    echo "The area of the rectagle is"." ".$Area;
+    echo "The sum of the rectangle is"." ".$sum;
+    >
+    </body>
+    </html>
